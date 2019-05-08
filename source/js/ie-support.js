e51f0af90239804975d44550ca0844a801a06e54
Ekaterina Zhuikova\kate <ezhuikova@otalio.com>
2019-05-08 15:43:15 +0300

    added modal

--- source/js/ie-support.js
+++ source/js/ie-support.js
@@ -1,9 +1,9 @@
 "use strict";
 
-const isIeBrowser = function () {
-  const ua = window.navigator.userAgent;
-  const msie = ua.indexOf('MSIE '); // IE 10 or older
-  const trident = ua.indexOf('Trident/'); //IE 11
+function isIeBrowser() {
+  const userAgent = window.navigator.userAgent;
+  const msie = userAgent.indexOf('MSIE '); // IE 10 or older
+  const trident = userAgent.indexOf('Trident/'); //IE 11
 
   return (msie > 0 || trident > 0);
 };
--- source/js/script.js
+++ source/js/script.js
@@ -9,7 +9,7 @@
 }());
 
 function toggleMenu() {
-  const navMain = document.querySelector(".page-header") || {};
+  const navMain = document.querySelector(".page-header") || {classList: []};
   const toggleButton = document.querySelector('.main-nav__toggle') || {};
 
   toggleButton.onclick = function () {
@@ -17,4 +17,39 @@ function toggleMenu() {
   }
 }
 
+function defineModalActions() {
+
+  const modal = document.querySelector('.modal--add-to-cart');
+  const modalOverlay = document.querySelector('.modal__overlay');
+  const openModalButtons = document.querySelectorAll(".open-add-to-cart-modal-js");
+
+  if (!modal || !modalOverlay) return;
+
+  Array.prototype.forEach.call(openModalButtons || [], function (button) {
+    button.addEventListener('click', function () {
+      showModal();
+    });
+  });
+
+  modalOverlay.onclick = function () {
+    closeModal();
+  };
+
+  const showModal = function () {
+    modalOverlay.classList.add('modal__overlay--show');
+    modal.classList.add('modal--show');
+  };
+
+  const closeModal = function () {
+    modalOverlay.classList.remove("modal__overlay--show");
+    modal.classList.remove("modal--show");
+  };
+
+  document.querySelector("form").addEventListener("submit", function (event) {
+    closeModal();
+    event.preventDefault();
+  });
+}
+
+defineModalActions();
 toggleMenu();
